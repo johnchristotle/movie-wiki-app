b5b76a8fae3d5d4b11bc6b7be767cb08d38f167e
agholor-christotle <agholorchristotle@gmail.com>
2022-11-20 17:01:30 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+
+import React, { useState, useEffect } from "react";
+
+import MovieCard from "./components/MovieCard";
+import SearchIcon from "./assets/search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=dade44e9";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies(searchTerm);
+  }, [searchTerm]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data)
+
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>LaChristotle Movie Wiki</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+          <p>Please type in the search box to see movies</p>
+        </div>
+      )}
+
+    </div>
+  );
+};
+
+export default App;
